c4df15997ae880f264eecc8dc662a293c0707846
oddcc <skycc71640@gmail.com>
2023-07-22 19:12:16 +0800

    Custom my dotfiles for macbook
    add execute privilege for mas
    
    update set-defaults
    
    disable keyboard repeat rate change
    
    update brew install apps

---
+++ homebrew/install-script.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+fi
+
+# Upgrade homebrew
+echo "› brew update"
+brew update
+
+brew tap afutured/openai-translator
+brew tap jesseduffield/lazydocker
+
+binaries=(
+  cloudflared
+  bitwarden-cli
+  ffmpeg
+  mas
+  neovim
+  jesseduffield/lazydocker/lazydocker
+  mackup
+  rclone
+  jenv
+  yt-dlp
+)
+
+echo "Installing binaries..."
+brew install ${binaries[@]}
+
+apps=(
+  # apps
+  adguard
+  anki
+  google-chrome
+  warp
+  openai-translator
+  figma
+  tor-browser
+  baidunetdisk
+  notion
+  obsidian
+  swift-quit
+  # system
+  input-source-pro
+  monitorcontrol
+  appcleaner
+  raycast
+  the-unarchiver
+  cloudflare-warp
+  caffeine
+  keyboard-cleaner
+  clashx-pro
+  # media
+  iina
+  plex
+  imazing
+  # dev
+  intellij-idea
+  visual-studio-code
+  orbstack
+  mockoon
+  postman
+  wireshark
+  cursor
+  input-source-pro
+)
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "Installing apps..."
+brew install --cask --appdir="/Applications" ${apps[@]}
+
+fonts=(
+  font-ibm-plex-mono
+  font-sarasa-gothic
+)
+
+echo "Installing fonts..."
+brew install ${fonts[@]}
+
+brew cleanup
+
+exit 0
---
+++ mas/install-script.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew); then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Check for mas-cli
+if test ! $(which mas); then
+  echo "  Installing mas-cli for you."
+
+  # Install mas-cli for you
+  brew install mas
+fi
+
+mas signout
+
+read -p " - Please sign in your us App Store account then press any key to continue..." ignore
+
+apps_us=(
+  985367838   # Microsoft Outlook
+  462054704   # Microsoft Word
+  462058435   # Microsoft Excel
+  462062816   # Microsoft PowerPoint
+  784801555   # Microsoft OneNote
+  1295203466  # Microsoft Remote Desktop
+  823766827   # OneDrive
+  922765270   # LiquidText
+  497799835   # Xcode
+  1475387142  # Tailscale
+  1352778147  # Bitwarden
+  1567748223  # SwiftyMenu
+  1548711022  # Barbee
+  1233965871  # ScreenBrush
+)
+
+echo "Installing apps for us account from mas-cli..."
+mas install ${apps_us[@]}
+mas signout
+
+read -p " - Please sign in your cn App Store account then press any key to continue..." ignore
+
+apps_cn=(
+  # 441258766   # Magnet
+  # 724472954   # Manico
+)
+
+echo "Installing apps for cn account from mas-cli..."
+mas install ${apps_cn[@]}
+mas signout
+
+exit 0
